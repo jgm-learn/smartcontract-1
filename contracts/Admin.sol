997e34111cd8fdec5fae79af3d28e9bf5e6114a3
jgm <1522953235.@qq.com>
2017-11-09 08:16:33 +0800

    完善admin合约

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.11;
 import "./ContractAddress.sol";
 import "./UserList.sol";
 import "./User.sol";
+import "./lib/LibString.sol";
 
 contract Admin
 {
@@ -11,14 +12,14 @@ contract Admin
         bytes32 user_id_;
         bytes32 user_sell_id_;
         uint    trade_id_;
-        bytes32 status;
+        bool    status_;
     }
     struct ConfirmNegReq
     {
         bytes32 user_id_;
         bytes32 user_sell_id_;
         uint    trade_id_;
-        bytes32 status;
+        bool    status_;
     }
 
     ContractAddress     contract_address;
@@ -36,12 +37,12 @@ contract Admin
 
     function insertConfirmListReq(bytes32 user_id, bytes32 user_sell_id,uint trade_id)
     {
-        confirm_list_req.push( ConfirmListReq(user_id,user_sell_id,trade_id));
+        confirm_list_req.push( ConfirmListReq(user_id,user_sell_id,trade_id,false));
     }
 
     function insertConfirmNegReq(bytes32 user_id, bytes32 user_sell_id, uint trade_id)
     {
-        confirm_neg_req.push( ConfirmNegReq(user_id,user_sell_id,trade_id));
+        confirm_neg_req.push( ConfirmNegReq(user_id,user_sell_id,trade_id,false));
     }
 
     //确认挂牌交易
@@ -51,6 +52,7 @@ contract Admin
         user_sell   = User(user_list.getUserAgentAddr(confirm_list_req[index].user_sell_id_));
         user.confirmList(confirm_list_req[index].trade_id_);
         user_sell.confirmList(confirm_list_req[index].trade_id_);
+        confirm_list_req[index].status_  =   true;
     }
 
     //确认协商交易
@@ -60,6 +62,7 @@ contract Admin
         user_sell   = User(user_list.getUserAgentAddr(confirm_neg_req[index].user_sell_id_));
         user.confirmNeg(confirm_neg_req[index].trade_id_);
         user_sell.confirmNeg(confirm_neg_req[index].trade_id_);
+        confirm_neg_req[index].status_   =   false;
     }
 
     //添加用户
@@ -85,5 +88,23 @@ contract Admin
     function getConfirmNegReqSize() returns(uint)
     {
         return confirm_neg_req.length; 
+    }
+
+    //获取挂牌交易确认请求列表的元素
+    function getConfirmListReq(uint index) returns(string user_id,string user_sell_id,uint trade_id,bool status)
+    {
+        user_id         =   LibString.bytes32ToString(confirm_list_req[index].user_id_);
+        user_sell_id    =   LibString.bytes32ToString(confirm_list_req[index].user_sell_id_);
+        trade_id        =   confirm_list_req[index].trade_id_;
+        status          =   confirm_list_req[index].status_;
+    }
+
+    //获取协商交易确认请求列表的元素
+    function getConfirmNegReq(uint index) returns(string user_id,string user_sell_id,uint trade_id,bool status)
+    {
+        user_id         =   LibString.bytes32ToString(confirm_neg_req[index].user_id_);
+        user_sell_id    =   LibString.bytes32ToString(confirm_neg_req[index].user_sell_id_);
+        trade_id        =   confirm_neg_req[index].trade_id_;
+        status          =   confirm_neg_req[index].status_;
     }
 }
